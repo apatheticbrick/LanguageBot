e762b54942c7bf8e60e764e8ed70f14426403f3a
ryaalbr <ryaalbr@nuevaschool.org>
2025-09-17 15:00:00 -0700

    added second page

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-let username;
-
 function submitInput() {
-    /*words = document.getElementById("chars_input").value;*/
-    /*email = document.getElementById("email_input").value;*/
-    /*exam_desc = document.getElementById("exam_desc_input").value;*/
+    words = document.getElementById("chars_input").value;
+    email = document.getElementById("email_input").value;
+    exam_desc = document.getElementById("exam_desc_input").value;
     document.getElementById("myH1").textContent = `Creating exam...`;
 
     const input_elements = ["chars_input", 
@@ -15,7 +13,23 @@ function submitInput() {
     ]
 
     input_elements.forEach(function (item, index) {
-        document.getElementById(item).style.visibility = 'hidden'
+        document.getElementById(item).style.display = 'none';
+    });
+
+    document.getElementById("email_label").textContent = email;
+    document.getElementById("chars_label").textContent = words;
+    document.getElementById("exam_desc_label").textContent = exam_desc;
+
+    const output_elements = ["info_label_1", 
+        "info_label_2",
+        "info_label_3",
+        "email_label", 
+        "chars_label", 
+        "exam_desc_label"
+    ]
+
+    output_elements.forEach(function (item, index) {
+        document.getElementById(item).style.display = 'block';
     });
 
     /* document.getElementById("chars_input").style.visibility = 'hidden';
